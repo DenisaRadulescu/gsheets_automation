363e2f8d56c9cd1bde85cdcbce3ab8e24837c9b9
DenisaRadulescu <denisa.r95@gmail.com>
2024-07-07 20:17:36 +0300

    add insert rows, modify cell, add worsheet and select worksheet functions in gsheet file

--- gsheets.py
+++ gsheets.py
@@ -13,16 +13,20 @@ class GoogleSheets:
         self.excel = self.client.open_by_key(excel_key)
         self.working_sheet = self.excel.sheet1
 
-    def insert_words(self):
-        pass
+    def insert_rows(self, values, index=1):
+        self.working_sheet.insert_row(values, index)
 
-    def modify_cell(self):
-        pass
+    def modify_cell(self, row, col, value):
+        self.working_sheet.update_cell(row, col, value)
 
     def get_values(self):
         return self.working_sheet.get_all_records()
 
+    def add_worksheet(self, title, rows=100, cols=20):
+        return self.excel.add_worksheet(title=title, rows=rows, cols=cols)
 
+    def select_worksheet(self, title):
+        self.working_sheet = self.excel.worksheet(title)
 
 # scopes = [
 #     "https://www.googleapis.com/auth/spreadsheets"
@@ -45,6 +49,11 @@ if __name__ == '__main__':
     excel = GoogleSheets(scopes=gsheets_config["scopes"],
                          credentials_path='credentials.json',
                          excel_key=gsheets_config["excel_id"])
+
+    excel.insert_rows(["Anca Popescu", "anca_popescu@example.com", "Marketing","-", "-", "01/01/2025"], index=5)
+    excel.modify_cell(1, 1, "Updated Name")
+    new_sheet = excel.add_worksheet("Departments")
+    excel.select_worksheet("Departments")
 
     employees = excel.get_values()
     print(employees)
